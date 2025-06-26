a6c999cebc0456bb399397b2a9c6d8ace451c3b0
Sahil Aftab <150364167+AftabSahil@users.noreply.github.com>
2025-06-26 20:49:07 +0530

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId=12345
+let accountEmail="abc@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+
+// accountId=2
+accountEmail="aaa@ss.com"
+accountPassword="123456"
+accountCity="Mumbai"
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity]);
